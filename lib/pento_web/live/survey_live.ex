198e5f2d68efcfa7348afc7a7164e494922b0a29
Pere Picó Muntaner <pere.pico@cabify.com>
2023-10-13 19:58:30 +0200

    Add a stateful component, DemographicLive.Form

--- lib/pento_web/live/survey_live.ex
+++ lib/pento_web/live/survey_live.ex
@@ -14,4 +14,14 @@ defmodule PentoWeb.SurveyLive do
   defp assign_demographic(%{assigns: %{current_user: current_user}} = socket) do
     assign(socket, :demographic, Survey.get_demographic_by_user(current_user))
   end
+
+  def handle_info({:created_demographic, demographic}, socket) do
+    {:noreply, handle_demographic_created(socket, demographic)}
+  end
+
+  defp handle_demographic_created(socket, demographic) do
+    socket
+    |> put_flash(:info, "Demographic created successfully")
+    |> assign(:demographic, demographic)
+  end
 end
