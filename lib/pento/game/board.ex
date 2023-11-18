bbd305deffc9114f3109e81b77c5791331ddc10e
Pere Picó Muntaner <pere.pico@cabify.com>
2023-11-18 21:45:46 +0100

    Add controls

--- lib/pento/game/board.ex
+++ lib/pento/game/board.ex
@@ -45,4 +45,75 @@ defmodule Pento.Game.Board do
 
   def active?(%{active_pento: %{name: shape_name}}, shape_name), do: true
   def active?(_board, _shape_name), do: false
+
+  @doc """
+  Pick allows the user to make a pento active. Clicking the board does nothing,
+  clicking an idle pento selects it and clicking the active pento releases it.
+  """
+  def pick(board, :board), do: board
+
+  def pick(%{active_pento: pento} = board, sname) when not is_nil(pento) do
+    if pento.name == sname do
+      %{board | active_pento: nil}
+    else
+      board
+    end
+  end
+
+  def pick(board, shape_name) do
+    active =
+      board.completed_pentos
+      |> Enum.find(&(&1.name == shape_name))
+      |> Kernel.||(new_pento(board, shape_name))
+
+    completed = Enum.filter(board.completed_pentos, &(&1.name != shape_name))
+
+    %{board | active_pento: active, completed_pentos: completed}
+  end
+
+  defp new_pento(board, shape_name) do
+    Pentomino.new(name: shape_name, location: midpoints(board))
+  end
+
+  defp midpoints(board) do
+    {x_coordinates, y_coordinates} = Enum.unzip(board.points)
+    {midpoint(x_coordinates), midpoint(y_coordinates)}
+  end
+
+  defp midpoint(numbers), do: round(Enum.max(numbers) / 2.0)
+
+  @doc """
+  `drop` handles the “drop the active pento” interaction.
+  If there’s nothing to drop, we return the board unchanged. For the valid drop scenario,
+  we first reset the board’s :active_pento attribute to nil. Then, we add the dropped pento
+  to the board’s list of :completed_pentos
+  """
+  def drop(%{active_pento: nil} = board), do: board
+
+  def drop(%{active_pento: pento} = board) do
+    board
+    |> Map.put(:active_pento, nil)
+    |> Map.put(:completed_pentos, [pento | board.completed_pentos])
+  end
+
+  @doc """
+  `legal_drop?` determines whether a drop is legal.
+  If any of the points from the existing pentominos are in the selected one, the move should be illegal.
+  Also, users can’t drop pentominos off of the board, meaning all active points need to be board points.
+  """
+  def legal_drop?(%{active_pento: pento}) when is_nil(pento), do: false
+
+  def legal_drop?(%{active_pento: pento, points: board_points} = board) do
+    points_on_board =
+      Pentomino.to_shape(pento).points
+      |> Enum.all?(fn point -> point in board_points end)
+
+    no_overlapping_pentos = !Enum.any?(board.completed_pentos, &Pentomino.overlapping?(pento, &1))
+
+    points_on_board and no_overlapping_pentos
+  end
+
+  def legal_move?(%{active_pento: pento, points: points} = _board) do
+    pento.location in points
+  end
 end
--- lib/pento_web/live/game_live/board.ex
+++ lib/pento_web/live/game_live/board.ex
@@ -3,7 +3,8 @@ defmodule PentoWeb.GameLive.Board do
 
   import PentoWeb.GameLive.{Colors, Component}
 
-  alias Pento.Game.{Board, Pentomino}
+  alias Pento.Game
+  alias Pento.Game.Board
 
   def update(%{puzzle: puzzle, id: id}, socket) do
     {:ok,
@@ -18,21 +19,10 @@ defmodule PentoWeb.GameLive.Board do
   end
 
   def assign_board(%{assigns: %{puzzle: puzzle}} = socket) do
-    active = Pentomino.new(name: :p, location: {7, 2})
-
-    completed = [
-      Pentomino.new(name: :u, rotation: 270, location: {1, 2}),
-      Pentomino.new(name: :v, rotation: 90, location: {4, 2})
-    ]
-
-    _puzzles = Board.puzzles()
-
     board =
       puzzle
       |> String.to_existing_atom()
       |> Board.new()
-      |> Map.put(:completed_pentos, completed)
-      |> Map.put(:active_pento, active)
 
     assign(socket, board: board)
   end
@@ -40,6 +30,47 @@ defmodule PentoWeb.GameLive.Board do
   def assign_shapes(%{assigns: %{board: board}} = socket) do
     shapes = Board.to_shapes(board)
     assign(socket, shapes: shapes)
+  end
+
+  def handle_event("pick", %{"name" => name}, socket) do
+    {:noreply, socket |> pick(name) |> assign_shapes}
+  end
+
+  def handle_event("key", %{"key" => key}, socket) do
+    {:noreply, socket |> do_key(key) |> assign_shapes}
+  end
+
+  def do_key(socket, key) do
+    case key do
+      " " -> drop(socket)
+      "ArrowLeft" -> move(socket, :left)
+      "ArrowRight" -> move(socket, :right)
+      "ArrowUp" -> move(socket, :up)
+      "ArrowDown" -> move(socket, :down)
+      "Shift" -> move(socket, :rotate)
+      "Enter" -> move(socket, :flip)
+      "Space" -> drop(socket)
+      _ -> socket
+    end
+  end
+
+  defp move(socket, move) do
+    case Game.maybe_move(socket.assigns.board, move) do
+      {:error, message} -> put_flash(socket, :info, message)
+      {:ok, board} -> socket |> assign(board: board) |> assign_shapes
+    end
+  end
+
+  defp drop(socket) do
+    case Game.maybe_drop(socket.assigns.board) do
+      {:error, message} -> put_flash(socket, :info, message)
+      {:ok, board} -> socket |> assign(board: board) |> assign_shapes()
+    end
+  end
+
+  defp pick(socket, name) do
+    shape_name = String.to_existing_atom(name)
+    update(socket, :board, &Board.pick(&1, shape_name))
   end
 
   def render(assigns) do
